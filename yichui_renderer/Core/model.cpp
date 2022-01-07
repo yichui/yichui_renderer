3c99c90203894fac9fe8f4fcb97e43b02aeca8fc
yichui_huan16 <896873144@qq.com>
2022-01-08 03:25:09 +0800

    second fouth chapter

--- yichui_renderer/Core/model.cpp
+++ yichui_renderer/Core/model.cpp
@@ -26,17 +26,31 @@ Model::Model(const char *filename) : verts_(), faces_() {
             Vec3f v;
             for (int i=0;i<3;i++) iss >> v[i];
             verts_.push_back(v);
-        } else if (!line.compare(0, 2, "f ")) {
-            std::vector<int> f;
+		}
+		else if (!line.compare(0, 3, "vn ")) {
+			iss >> trash >> trash;
+			Vec3f n;
+			for (int i = 0; i < 3; i++) iss >> n[i];
+			norms_.push_back(n);
+		}
+		else if (!line.compare(0, 3, "vt ")) {
+			iss >> trash >> trash;
+			Vec2f uv;
+			for (int i = 0; i < 2; i++) iss >> uv[i];
+			uv_.push_back(uv);
+		}
+		else if (!line.compare(0, 2, "f ")) {
+            std::vector<Vec3i> f;
+            Vec3i tmp;
             int itrash, idx;
             iss >> trash;
 			//此处的循环需要读取的是x/x/x中第一个数字，代表三维坐标轴数据，所以通过下述while条件即可每次获取idx内容。
             //通过改变一定的顺序即可获取其他位置的数字，比如第二个x就是我们需要的材质文件像素点vt坐标。
-            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                idx--; // in wavefront obj all indices start at 1, not zero
-                f.push_back(idx);
-            }
-            faces_.push_back(f);
+			while (iss >> tmp[0] >> trash >> tmp[1] >> trash >> tmp[2]) {
+				for (int i = 0; i < 3; i++) tmp[i]--; // in wavefront obj all indices start at 1, not zero
+				f.push_back(tmp);
+			}
+			faces_.push_back(f);
         }
     }
     std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
@@ -54,7 +68,9 @@ int Model::nfaces() {
 }
 
 std::vector<int> Model::face(int idx) {
-    return faces_[idx];
+	std::vector<int> face;
+	for (int i = 0; i < (int)faces_[idx].size(); i++) face.push_back(faces_[idx][i][0]);
+	return face;
 }
 
 Vec3f Model::vert(int i) {
