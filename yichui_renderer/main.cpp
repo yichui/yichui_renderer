2bb78283967a9695642933b5f99682608487314d
yichui_ty <896873144@qq.com>
2022-01-19 23:37:27 +0800

    chapter 6+

--- yichui_renderer/main.cpp
+++ yichui_renderer/main.cpp
@@ -12,6 +12,28 @@ Vec3f       eye(1, 1, 3);
 Vec3f    center(0, 0, 0);
 Vec3f        up(0, 1, 0);
 
+struct Shader : public IShader {
+    mat<2,3,float> varying_uv;  // triangle uv coordinates, written by the vertex shader, read by the fragment shader
+    mat<3,3,float> varying_nrm; // normal per vertex to be interpolated by FS
+
+    virtual Vec4f vertex(int iface, int nthvert) {
+        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        varying_nrm.set_col(nthvert, proj<3>((Projection*ModelView).invert_transpose()*embed<4>(model->normal(iface, nthvert), 0.f)));
+        Vec4f gl_Vertex = Projection*ModelView*embed<4>(model->vert(iface, nthvert));
+        varying_tri.set_col(nthvert, gl_Vertex);
+        return gl_Vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        Vec3f bn = (varying_nrm*bar).normalize();
+        Vec2f uv = varying_uv*bar;
+
+        float diff = std::max(0.f, bn*light_dir);
+        color = model->diffuse(uv)*diff;
+        return false;
+    }
+};
+
 struct PhongShader : public IShader {
 	Vec3f          varying_intensity; // written by vertex shader, read by fragment shader
 	mat<2, 3, float> varying_uv;        // same as above
